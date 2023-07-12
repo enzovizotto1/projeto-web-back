ca15de6ed567eb20711880557d44025e3b6ecc01
unknown <enzovizotto57@gmail.com>
2023-07-12 04:36:22 -0300

    Steam API

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-import express from "express";
+import express from "express"
+import steamRouter from "./Routes/steam.routes.js"
+
+
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use('/public', express.static('public'))
+
+app.use("/jogo", steamRouter)
+
+app.listen(port, () => {
+    console.log(`A nossa API está rodando na porta ${port}`)
+  })
+  
