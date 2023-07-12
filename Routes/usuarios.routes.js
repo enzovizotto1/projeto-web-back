7c9d13aa793c1dc6ccf7f3edcb3b3833c872a716
Gustavo Gimenez <ggimenezt@gmail.com>
2023-07-12 05:38:55 -0300

    rotas de usuarios

---
+++ Routes/usuarios.routes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import multer from "multer"; //para foto de perfil
+import path from "path"; //para foto de perfil
+import * as usuariosController from '../Controllers/usuarios.controller.js';
+import autorizarUsuario from "../Middlewares/auth.middleware.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public")
+    },
+    filename:  function (req, file, cb) {
+        cb(null, Date.now()+path.extname(file.originalname));
+    }
+});
+
+const router = express.Router();
+const upload = multer({ storage }).single('foto_de_perfil');
+
+//CRUD
+// Criação, atuaalização, remoção, retornar todos usuários e retornar um usuário especifico
+
+router.post('/', usuariosController.criarUsuario)
+router.post('/login', usuariosController.login)
+router.put('/:usuarioId', upload, usuariosController.atualizarUsuario)
+router.delete('/:usuarioId', usuariosController.deletarUsuario)
+router.get('/', usuariosController.getUsuarios)
+router.get('/:usuarioId', usuariosController.getUsuarioPorId)
+router.put('/adicionar/:usuarioId', usuariosController.adicionarAmigo)
+
+export default router;
--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 import express from "express";
+import usuariosRouter from './Routes/usuarios.routes.js';
+
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use('/usuarios', usuariosRouter)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
