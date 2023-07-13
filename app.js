a112cb5f7c7e4440fd37692124453dfdd7f7975c
unknown <enzovizotto57@gmail.com>
2023-07-13 02:30:30 -0300

    adicionando cors

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from "express";
 import steamRouter from "./Routes/steam.routes.js"
 import usuariosRouter from './Routes/usuarios.routes.js'
 import ratingRouter from './Routes/rating.routes.js';
+import cors from "cors"
 
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(express.json())
+
+app.use(cors());
 
 app.use('/public', express.static('public'))
 app.use('/usuarios', usuariosRouter)
