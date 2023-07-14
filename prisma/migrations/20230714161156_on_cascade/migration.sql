55249002d37ffeb1aa927053371e5b6cb5d9743f
Gustavo Gimenez <ggimenezt@gmail.com>
2023-07-14 13:24:31 -0300

    Update migration.sql

--- prisma/migrations/20230714161156_on_cascade/migration.sql
+++ prisma/migrations/20230714161156_on_cascade/migration.sql
@@ -1,8 +1,8 @@
 -- DropForeignKey
-ALTER TABLE `avaliacao` DROP FOREIGN KEY `Avaliacao_jogoId_fkey`;
+ALTER TABLE `Avaliacao` DROP FOREIGN KEY `Avaliacao_jogoId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `avaliacao` DROP FOREIGN KEY `Avaliacao_usuarioId_fkey`;
+ALTER TABLE `Avaliacao` DROP FOREIGN KEY `Avaliacao_usuarioId_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `Avaliacao` ADD CONSTRAINT `Avaliacao_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `Usuario`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
