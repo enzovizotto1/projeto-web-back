dd5c03e19b3d74f03eadcd3f9a2d965b1d8a0573
unknown <enzovizotto57@gmail.com>
2023-07-14 13:12:28 -0300

    arrumando imagens e delete

--- Controllers/jogos.controller.js
+++ Controllers/jogos.controller.js
@@ -11,11 +11,11 @@ export async function popularJogos() {
             return
         } else{
             const jogos = [
-                {"nome": "Zelda", "foto": "../public/zelda.png", "sinopse": "O game se passa no reino de Hyrule, no qual o jovem guerreiro Link tem a missão de salvar seu povo, a Princesa Zelda e proteger a Triforce, símbolo deixado por deusas capaz de trazer ao mundo uma era de harmonia e prosperidade."},
-                {"nome": "CSGO", "foto": "../public/csgo.png", "sinopse": "CS:GO é um FPS da Valve disputado entre duas equipes. Elas se revezam entre terroristas e contra terroristas, os terroristas armam a bomba enquanto o outro time protege os sites, locais onde os explosivos devem ser plantados. Alguns mapas funcionam com reféns, que por sua vez devem ser salvos pelos contra terroristas."},
-                {"nome": "Mario", "foto": "../public/mario.png", "sinopse": "Mario Bros retrata as aventuras de Mario, um encanador, e seus amigos, que se unem em torno de um objetivo em comum: salvar a princesa Peach, capturada pelo terrível monstro Bowser Koppa."},
-                {"nome": "Valorant", "foto": "../public/valorant.png", "sinopse": "Você terá 13 rodadas para atacar e defender com disparos certeiros e habilidades táticas. Com apenas uma vida por rodada, você deve pensar mais rápido que o oponente se quiser sobreviver. Encare inimigos nos modos Competitivo e Sem Ranque, além da Disputa da Spike e do Mata-Mata!"},
-                {"nome": "Sonic", "foto": "../public/mario.png", "sinopse": "O porco-espinho Sonic é teletransportado para a Terra. Após causar uma pane elétrica em parte dos Estados Unidos, ele precisa escapar do maligno cientista Dr. Robotnik."}
+                {"nome": "Zelda", "foto": "zelda.png", "sinopse": "O game se passa no reino de Hyrule, no qual o jovem guerreiro Link tem a missão de salvar seu povo, a Princesa Zelda e proteger a Triforce, símbolo deixado por deusas capaz de trazer ao mundo uma era de harmonia e prosperidade."},
+                {"nome": "CSGO", "foto": "csgo.png", "sinopse": "CS:GO é um FPS da Valve disputado entre duas equipes. Elas se revezam entre terroristas e contra terroristas, os terroristas armam a bomba enquanto o outro time protege os sites, locais onde os explosivos devem ser plantados. Alguns mapas funcionam com reféns, que por sua vez devem ser salvos pelos contra terroristas."},
+                {"nome": "Mario", "foto": "mario.png", "sinopse": "Mario Bros retrata as aventuras de Mario, um encanador, e seus amigos, que se unem em torno de um objetivo em comum: salvar a princesa Peach, capturada pelo terrível monstro Bowser Koppa."},
+                {"nome": "Valorant", "foto": "valorant.png", "sinopse": "Você terá 13 rodadas para atacar e defender com disparos certeiros e habilidades táticas. Com apenas uma vida por rodada, você deve pensar mais rápido que o oponente se quiser sobreviver. Encare inimigos nos modos Competitivo e Sem Ranque, além da Disputa da Spike e do Mata-Mata!"},
+                {"nome": "Sonic", "foto": "mario.png", "sinopse": "O porco-espinho Sonic é teletransportado para a Terra. Após causar uma pane elétrica em parte dos Estados Unidos, ele precisa escapar do maligno cientista Dr. Robotnik."}
             ]
 
             for (const jogo of jogos) {
---
+++ prisma/migrations/20230714161156_on_cascade/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE `avaliacao` DROP FOREIGN KEY `Avaliacao_jogoId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `avaliacao` DROP FOREIGN KEY `Avaliacao_usuarioId_fkey`;
+
+-- AddForeignKey
+ALTER TABLE `Avaliacao` ADD CONSTRAINT `Avaliacao_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `Usuario`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Avaliacao` ADD CONSTRAINT `Avaliacao_jogoId_fkey` FOREIGN KEY (`jogoId`) REFERENCES `Jogo`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
